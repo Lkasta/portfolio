f0a33f6a0ba311b47039d26ed673045276f54d82
lkasta <lkastabackup@gmail>
2022-11-29 22:18:02 -0300

    Deenhando site 1.11 e adicionando funcoes 1.0

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function itemMenuLeftSide() {
+    var titleText = document.querySelector(".title-link")
+    var backgroundText = document.querySelector(".img-link")
+    titleText.classList.toggle("active")
+    backgroundText.classList.toggle("active")
+}
