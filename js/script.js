798f295e5bcad3bd5de44655fd919389b6d0846e
lkasta <lkastabackup@gmail.com>
2022-12-15 17:24:23 -0300

    Update functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-function itemMenuLeftSide() {
-    var titleText = document.querySelector(".title-link")
-    var backgroundText = document.querySelector(".img-link")
-    titleText.classList.toggle("active")
-    backgroundText.classList.toggle("active")
-}
+// function itemMenuLeftSide() {
+//     var titleText = document.querySelector(".title-link")
+//     var backgroundText = document.querySelector(".img-link")
+//     titleText.classList.toggle("active")
+//     backgroundText.classList.toggle("active")
+// }
+
+document.addEventListener('click', (e) => {
+    const targetEl = e.target;
+    const irma = targetEl.previousSibling;;
+    const elementoPai = targetEl.parentNode
+
+    console.log(targetEl)
+    console.log(elementoPai)
+    console.log(irma)
+
+
+    if(elementoPai.classList.contains('pai')) {
+        elementoPai.classList.toggle("active")
+    }
+})
